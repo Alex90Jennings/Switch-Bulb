219a74a135a9ee0d557ab355a4c0350be52fec6d
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2022-09-01 13:28:32 +0200

    H1 now appears above the lightbulb

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const btnOff = document.querySelector(".btn-off");
 const btnOn = document.querySelector(".btn-on");
 const container = document.querySelector(".light-container");
+const headerContainer = document.querySelector(".header-container");
 
 // Swith ON + Add sentence once light is switched on
 function bulbOn() {
   const textAdd = document.createElement("h1");
   textAdd.innerText = "I have an idea!";
-  container.append(textAdd);
+  textAdd.setAttribute("class", "centered")
+  headerContainer.prepend(textAdd);
 
   const lightOn = document.querySelector(".bulb-off");
   lightOn.setAttribute("src", "BulbOn.png");
